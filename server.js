e95fd4681c6549e1b03d427671d3f5e57daf9920
pumpsomekins <86573495+pumpsomekins@users.noreply.github.com>
2023-10-20 18:03:57 -0400

    sdafaf

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     if (req){
         console.log('hello');
     }
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('hello, ninjas!!!!');
-    res.end();
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('hello, ninjas!!!!');
+    // res.write('<p>hello, Ninja</p>');
+    // res.end();
+
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end();
+        }
+        else {
+            // res.write(data);
+            res.statusCode = 200;
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
